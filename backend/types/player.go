d101638fdbd3527c2f6a48fee5d7f878fbb61b29
flerner <felix.lerner01@gmail.com>
2024-02-13 14:06:45 -0300

    refactor: types

--- backend/types/player.go
+++ backend/types/player.go
@@ -1,9 +1,5 @@
 package types
 
-import (
-	"encoding/json"
-)
-
 type Player struct {
 	position Position
 }
@@ -12,15 +8,4 @@ func (p Player) CreatePlayer(x, z float32) Player {
 	return Player{
 		position: Position{X: x, Z: z},
 	}
-}
-func CreatePosition(bytes []byte) Position{
-	pos := Position{}
-	if err := json.Unmarshal(bytes, &pos); err != nil {
-        panic(err)
-    }
-	return pos
-}
-type Position struct {
-	X float32 `json:"x"`
-	Z float32 `json:"z"`
 }
---
+++ backend/types/position.go
@@ -0,0 +1,17 @@
+package types
+
+import (
+	"encoding/json"
+)
+
+type Position struct {
+	X float32 `json:"x"`
+	Z float32 `json:"z"`
+}
+func CreatePosition(bytes []byte) Position{
+	pos := Position{}
+	if err := json.Unmarshal(bytes, &pos); err != nil {
+        panic(err)
+    }
+	return pos
+}
