f669c9af71e4692fef2e1ea860ecc949b0dbe8ab
Theo <theokatz@gmail.com>
2024-07-07 14:55:45 -0300

    trigger attack animation all logic on client

--- client/Assets/Scripts/MainPlayer.cs
+++ client/Assets/Scripts/MainPlayer.cs
@@ -68,6 +68,7 @@ public class MainPlayer : MonoBehaviour
             };
 			string message = JsonConvert.SerializeObject(response);
 			WebSocketConnection.SendMessage(message);
+            PlayerMovement.Instance().AttackAnimation();
         }
     }
 }
