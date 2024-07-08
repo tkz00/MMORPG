2166b7211092c60ab7a8ef0669ee8956aca9ec23
Theo <theokatz@gmail.com>
2024-07-07 21:17:51 -0300

    fix heal targeting and remove logs

--- client/Assets/Scripts/MainPlayer.cs
+++ client/Assets/Scripts/MainPlayer.cs
@@ -88,11 +88,9 @@ public class MainPlayer : MonoBehaviour
 
     private void CastHealAbility(InputAction.CallbackContext context)
     {
-        Debug.Log("asdkjhasdkjah");
         Ray ray = mainCamera.ScreenPointToRay(Mouse.current.position.ReadValue());
         if(Physics.Raycast(ray, out RaycastHit hit, Mathf.Infinity, playersLayer) && hit.collider) {
             
-            Debug.Log("merge error");
             string targetId = hit.collider.GetComponent<ITargeteable>().GetTargetId();
             Dictionary<AbilityParameters, object> abilityParameters =
                 new Dictionary<AbilityParameters, object>{
@@ -107,7 +105,6 @@ public class MainPlayer : MonoBehaviour
                 ActionType = "AbilityCast"
             };
             string message = JsonConvert.SerializeObject(response);
-            Debug.Log(message);
             WebSocketConnection.SendMessage(message);
         }
     }
