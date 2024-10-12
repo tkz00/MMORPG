5257f26fe2ec2abec7be9e13a2be5cfc7e4a662a
Theo <theokatz@gmail.com>
2024-10-12 19:47:18 -0300

    render characters panels on top of world elements

--- client/Assets/Scripts/PlayerPanelsUI/PlayerPanel.cs
+++ client/Assets/Scripts/PlayerPanelsUI/PlayerPanel.cs
@@ -8,11 +8,16 @@ public class PlayerPanel : MonoBehaviour
 {
 	Transform cameraTransform;
 
-	void Start() {
-		cameraTransform = Camera.main.transform;
+	void Start()
+	{
+		Camera camera = Camera.main;
+		cameraTransform = camera.transform;
+		// From this answer this should select the UI camera, but with the main camera it works (https://discussions.unity.com/t/world-space-canvas-on-top-of-everything/128165/3)
+		GetComponent<Canvas>().worldCamera = camera;
 	}
 
-	void LateUpdate() {
+	void LateUpdate()
+	{
 		transform.LookAt(transform.position + cameraTransform.forward);
 	}
 }
