1b24d04cfcd445c3188060802832cf67c632b83f
tkz00 <theokatz@gmail.com>
2025-01-13 21:05:54 -0300

    get and patch abilities methods

--- backend/pkg/configurator/configurator.go
+++ backend/pkg/configurator/configurator.go
@@ -16,59 +16,9 @@ const ABILITIES_FILE_NAME = "abilities.json"
 func SetupRouter() *gin.Engine {
 	r := gin.Default()
 
-	r.GET("/abilities", func(c *gin.Context) {
-		abilities, err := loadAbilitiesFromFile(ABILITIES_FILE_NAME)
-		if err != nil {
-			fmt.Printf("Error loading abilities: %v\n", err)
-			return
-		}
+	r.GET("/abilities", GetAbilities)
 
-		c.JSON(http.StatusOK, gin.H{
-			"abilities": abilities,
-		})
-	})
-
-	r.PATCH("/ability/:id", func(c *gin.Context) {
-		id := c.Param("id")
-		abilities, err := loadAbilitiesFromFile(ABILITIES_FILE_NAME)
-		if err != nil {
-			fmt.Printf("Error loading abilities: %v\n", err)
-			return
-		}
-
-		if ability, ok := abilities[id]; ok {
-			fmt.Println(ability)
-			updatedAbility := ConfiguratorAbility{}
-			if err := c.BindJSON(&updatedAbility); err != nil {
-				c.AbortWithError(http.StatusBadRequest, err)
-				return
-			}
-
-			if updatedAbility.Name != "" {
-				ability.Name = updatedAbility.Name
-			}
-
-			if updatedAbility.Cooldown != 0 {
-				ability.Cooldown = updatedAbility.Cooldown
-			}
-
-			if updatedAbility.RangeValue != 0 {
-				ability.RangeValue = updatedAbility.RangeValue
-			}
-
-			if updatedAbility.Targeting != nil {
-				ability.Targeting = updatedAbility.Targeting
-			}
-
-			abilities[id] = ability
-			saveAbilitiesToFile(abilities, ABILITIES_FILE_NAME)
-			c.JSON(http.StatusAccepted, &ability)
-			return
-		}
-
-		// If ability not found
-		c.JSON(http.StatusNotFound, gin.H{"message": "ability not found"})
-	})
+	r.PATCH("/ability/:id", UpdateAbility)
 	return r
 }
 
@@ -84,6 +34,60 @@ func Run() {
 	r := SetupRouter()
 	fmt.Println("Starting server on port 8080...")
 	r.Run()
+}
+
+func GetAbilities(c *gin.Context) {
+	abilities, err := loadAbilitiesFromFile(ABILITIES_FILE_NAME)
+	if err != nil {
+		fmt.Printf("Error loading abilities: %v\n", err)
+		return
+	}
+
+	c.JSON(http.StatusOK, gin.H{
+		"abilities": abilities,
+	})
+}
+
+func UpdateAbility(c *gin.Context) {
+	id := c.Param("id")
+	abilities, err := loadAbilitiesFromFile(ABILITIES_FILE_NAME)
+	if err != nil {
+		fmt.Printf("Error loading abilities: %v\n", err)
+		return
+	}
+
+	if ability, ok := abilities[id]; ok {
+		fmt.Println(ability)
+		updatedAbility := ConfiguratorAbility{}
+		if err := c.BindJSON(&updatedAbility); err != nil {
+			c.AbortWithError(http.StatusBadRequest, err)
+			return
+		}
+
+		if updatedAbility.Name != "" {
+			ability.Name = updatedAbility.Name
+		}
+
+		if updatedAbility.Cooldown != 0 {
+			ability.Cooldown = updatedAbility.Cooldown
+		}
+
+		if updatedAbility.RangeValue != 0 {
+			ability.RangeValue = updatedAbility.RangeValue
+		}
+
+		if updatedAbility.Targeting != nil {
+			ability.Targeting = updatedAbility.Targeting
+		}
+
+		abilities[id] = ability
+		saveAbilitiesToFile(abilities, ABILITIES_FILE_NAME)
+		c.JSON(http.StatusAccepted, &ability)
+		return
+	}
+
+	// If ability not found
+	c.JSON(http.StatusNotFound, gin.H{"message": "ability not found"})
 }
 
 func saveAbilitiesToFile(abilities map[string]ConfiguratorAbility, filename string) error {
