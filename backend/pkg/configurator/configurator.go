ed155ee877610b381b6efd5a6925c648656d0e54
tkz00 <theokatz@gmail.com>
2025-03-03 23:25:26 -0300

    clean up backend code

--- backend/pkg/configurator/configurator.go
+++ backend/pkg/configurator/configurator.go
@@ -1,18 +1,13 @@
 package configurator
 
 import (
-	"encoding/json"
 	"errors"
 	"fmt"
 	"net/http"
-	"os"
 	"strconv"
-	"tkz00/backend/pkg/game/entities"
 
 	"github.com/gin-gonic/gin"
 )
-
-const ABILITIES_FILE_NAME = "abilities.json"
 
 func SetupRouter() *gin.Engine {
 	r := gin.Default()
@@ -32,7 +27,7 @@ func RunSeeds() {
 		configuratorAbilities[i] = ConvertToConfiguratorAbility(*ability)
 	}
 
-	SaveAbilitiesToFile(configuratorAbilities, ABILITIES_FILE_NAME)
+	SaveAbilitiesToFile(configuratorAbilities)
 }
 
 func Run() {
@@ -42,7 +37,7 @@ func Run() {
 }
 
 func GetAbilities(c *gin.Context) {
-	abilities, err := LoadAbilitiesFromFile(ABILITIES_FILE_NAME)
+	abilities, err := LoadAbilitiesFromFile()
 	if err != nil {
 		fmt.Printf("Error loading abilities: %v\n", err)
 		return
@@ -55,7 +50,7 @@ func GetAbilities(c *gin.Context) {
 
 func GetAbility(c *gin.Context) {
 	id := c.Param("id")
-	abilities, err := LoadAbilitiesFromFile(ABILITIES_FILE_NAME)
+	abilities, err := LoadAbilitiesFromFile()
 	if err != nil {
 		fmt.Printf("Error loading abilities: %v\n", err)
 		return
@@ -70,200 +65,114 @@ func GetAbility(c *gin.Context) {
 }
 
 func CreateAbility(c *gin.Context) {
-	abilities, err := LoadAbilitiesFromFile(ABILITIES_FILE_NAME)
+	var newAbility ConfiguratorAbility
+	if err := c.ShouldBindJSON(&newAbility); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+	if err := validateAbility(newAbility); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+
+	abilities, err := LoadAbilitiesFromFile()
 	if err != nil {
 		fmt.Printf("Error loading abilities: %v\n", err)
 		return
 	}
 
-	newAbility := ConfiguratorAbility{}
-	if err := c.BindJSON(&newAbility); err != nil {
-		c.AbortWithError(http.StatusBadRequest, err)
-		return
-	}
-
-	if newAbility.Name == "" {
-		c.AbortWithError(http.StatusBadRequest, errors.New("`name` required"))
-		c.JSON(http.StatusBadRequest, gin.H{
-			"error": "`name` required",
-		})
-		return
-	}
-
-	if newAbility.Cooldown == nil {
-		c.AbortWithError(http.StatusBadRequest, errors.New("`cooldown` required"))
-		c.JSON(http.StatusBadRequest, gin.H{
-			"error": "`cooldown` required",
-		})
-		return
-	}
-
-	if newAbility.RangeValue == nil {
-		c.AbortWithError(http.StatusBadRequest, errors.New("`range` required"))
-		c.JSON(http.StatusBadRequest, gin.H{
-			"error": "`range` required",
-		})
-		return
-	}
-
-	if newAbility.Targeting == nil {
-		c.AbortWithError(http.StatusBadRequest, errors.New("`targeting` required"))
-		c.JSON(http.StatusBadRequest, gin.H{
-			"error": "`targeting` required",
-		})
-		return
-	}
-
-	if newAbility.CharacterState == nil {
-		c.AbortWithError(http.StatusBadRequest, errors.New("`character_state` required"))
-		c.JSON(http.StatusBadRequest, gin.H{
-			"error": "`character_state` required",
-		})
-		return
-	}
-
-	lastId := -1
-	for idStr := range abilities {
-		id, _ := strconv.Atoi(idStr)
-		if id > lastId {
-			lastId = id
-		}
-	}
-
-	lastIdStr := strconv.Itoa(lastId + 1)
-	newAbility.ID = lastIdStr
-	abilities[lastIdStr] = newAbility
-	SaveAbilitiesToFile(abilities, ABILITIES_FILE_NAME)
-	c.JSON(http.StatusAccepted, newAbility)
+	newID := getNextAbilityID(abilities)
+	newAbility.ID = newID
+	abilities[newID] = newAbility
+	SaveAbilitiesToFile(abilities)
+	c.JSON(http.StatusCreated, newAbility)
 }
 
 func UpdateAbility(c *gin.Context) {
 	id := c.Param("id")
-	abilities, err := LoadAbilitiesFromFile(ABILITIES_FILE_NAME)
+	abilities, err := LoadAbilitiesFromFile()
 	if err != nil {
-		fmt.Printf("Error loading abilities: %v\n", err)
+		c.JSON(http.StatusInternalServerError, gin.H{"error": "failed to load abilities"})
+		return
+	}
+	ability, exists := abilities[id]
+	if !exists {
+		c.JSON(http.StatusNotFound, gin.H{"error": "ability not found"})
 		return
 	}
 
-	if ability, ok := abilities[id]; ok {
-		updatedAbility := ConfiguratorAbility{}
-		if err := c.BindJSON(&updatedAbility); err != nil {
-			c.AbortWithError(http.StatusBadRequest, err)
-			return
-		}
-
-		if updatedAbility.Name != "" {
-			ability.Name = updatedAbility.Name
-		}
-
-		if updatedAbility.Cooldown != nil {
-			ability.Cooldown = updatedAbility.Cooldown
-		}
-
-		if updatedAbility.RangeValue != nil {
-			ability.RangeValue = updatedAbility.RangeValue
-		}
-
-		if updatedAbility.Targeting != nil {
-			ability.Targeting = updatedAbility.Targeting
-		}
-
-		if updatedAbility.CharacterState != nil {
-			ability.CharacterState = updatedAbility.CharacterState
-		}
-
-		if len(updatedAbility.Mechanics) > 0 {
-			ability.Mechanics = updatedAbility.Mechanics
-		}
-
-		abilities[id] = ability
-		SaveAbilitiesToFile(abilities, ABILITIES_FILE_NAME)
-		c.JSON(http.StatusAccepted, ability)
+	updatedAbility := ConfiguratorAbility{}
+	if err := c.BindJSON(&updatedAbility); err != nil {
+		c.AbortWithError(http.StatusBadRequest, err)
 		return
 	}
 
-	c.JSON(http.StatusNotFound, gin.H{"message": "ability not found"})
+	if updatedAbility.Name != "" {
+		ability.Name = updatedAbility.Name
+	}
+	if updatedAbility.Cooldown != nil {
+		ability.Cooldown = updatedAbility.Cooldown
+	}
+	if updatedAbility.RangeValue != nil {
+		ability.RangeValue = updatedAbility.RangeValue
+	}
+	if updatedAbility.Targeting != nil {
+		ability.Targeting = updatedAbility.Targeting
+	}
+	if updatedAbility.CharacterState != nil {
+		ability.CharacterState = updatedAbility.CharacterState
+	}
+	if len(updatedAbility.Mechanics) > 0 {
+		ability.Mechanics = updatedAbility.Mechanics
+	}
+
+	abilities[id] = ability
+	SaveAbilitiesToFile(abilities)
+	c.JSON(http.StatusAccepted, ability)
 }
 
 func DeleteAbility(c *gin.Context) {
 	id := c.Param("id")
-	abilities, err := LoadAbilitiesFromFile(ABILITIES_FILE_NAME)
+	abilities, err := LoadAbilitiesFromFile()
 	if err != nil {
-		fmt.Printf("Error loading abilities: %v\n", err)
+		c.JSON(http.StatusInternalServerError, gin.H{"error": "failed to load abilities"})
 		return
 	}
 
-	if _, ok := abilities[id]; ok {
+	if _, exists := abilities[id]; exists {
 		delete(abilities, id)
-		SaveAbilitiesToFile(abilities, ABILITIES_FILE_NAME)
-		c.JSON(http.StatusOK, "")
+		SaveAbilitiesToFile(abilities)
+		c.JSON(http.StatusOK, gin.H{"message": "ability deleted"})
 		return
 	}
-
-	c.JSON(http.StatusNotFound, gin.H{"message": "ability not found"})
+	c.JSON(http.StatusNotFound, gin.H{"error": "ability not found"})
 }
 
-func SaveAbilitiesToFile(abilities map[string]ConfiguratorAbility, filename string) error {
-	file, err := os.OpenFile(filename, os.O_CREATE|os.O_WRONLY|os.O_TRUNC, 0644)
-	if err != nil {
-		return fmt.Errorf("error opening file: %v", err)
+func validateAbility(a ConfiguratorAbility) error {
+	if a.Name == "" {
+		return errors.New("`name` required")
 	}
-	defer file.Close()
-
-	encoder := json.NewEncoder(file)
-	encoder.SetIndent("", "  ")
-	err = encoder.Encode(abilities)
-	if err != nil {
-		return fmt.Errorf("error encoding abilities to JSON: %v", err)
+	if a.Cooldown == nil {
+		return errors.New("`cooldown` required")
 	}
-
+	if a.RangeValue == nil {
+		return errors.New("`range` required")
+	}
+	if a.Targeting == nil {
+		return errors.New("`targeting` required")
+	}
+	if a.CharacterState == nil {
+		return errors.New("`character_state` required")
+	}
 	return nil
 }
 
-func LoadAbilitiesFromFile(filename string) (map[string]ConfiguratorAbility, error) {
-	file, err := os.Open(filename)
-	if err != nil {
-		return nil, fmt.Errorf("error opening file: %v", err)
+func getNextAbilityID(abilities map[string]ConfiguratorAbility) string {
+	maxID := 0
+	for id := range abilities {
+		if num, err := strconv.Atoi(id); err == nil && num > maxID {
+			maxID = num
+		}
 	}
-	defer file.Close()
-
-	abilities := make(map[string]ConfiguratorAbility)
-
-	decoder := json.NewDecoder(file)
-	err = decoder.Decode(&abilities)
-	if err != nil {
-		return nil, fmt.Errorf("error decoding abilities from JSON: %v", err)
-	}
-
-	return abilities, nil
-}
-
-// ConfiguratorAbility struct for API response
-type ConfiguratorAbility struct {
-	ID             string              `json:"id"`
-	Name           string              `json:"name"`
-	RangeValue     *float64            `json:"range"`
-	Cooldown       *int64              `json:"cooldown"`
-	Targeting      *entities.Targeting `json:"targeting"`
-	CharacterState *entities.Action    `json:"character_state"`
-	Mechanics      []entities.Mechanic `json:"mechanics"`
-}
-
-// Convert function
-func ConvertToConfiguratorAbility(ability entities.Ability) ConfiguratorAbility {
-	targeting := entities.Targeting(
-		ability.Targeting(),
-	) // I don't know why I have to do this, but it doesn't work otherwise
-	rangeValue := ability.Range()
-	cooldown := ability.Cooldown()
-	characterState := ability.CharacterState()
-	return ConfiguratorAbility{
-		ID:             ability.Id(),
-		Name:           ability.Name(),
-		RangeValue:     &rangeValue,
-		Cooldown:       &cooldown,
-		Targeting:      &targeting,
-		CharacterState: &characterState,
-		Mechanics:      ability.Mechanics(),
-	}
+	return strconv.Itoa(maxID + 1)
 }
